6a261563dd52e43202ab23f2eb4b9b927bdaa608
Facundo Javier Gelatti <javiergelatti@gmail.com>
2021-05-16 17:54:33 -0300

    Improve testing result reports

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+#set -euo pipefail
 
-bundle exec arduino_ci.rb --skip-examples-compilation
+(bundle exec arduino_ci.rb --skip-examples-compilation \
+    && printf "\n🟢 \033[92mPASS\033[0m\n") || printf "\n🔴 \033[91mFAIL\033[0m\n"
