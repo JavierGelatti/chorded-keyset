6892784944964752f9de7ab2b0c506d12a224300
Facundo Javier Gelatti <javiergelatti@gmail.com>
2021-05-17 03:15:10 -0300

    Add more commands

--- hud.rb
+++ hud.rb
@@ -3,6 +3,10 @@ require 'io/console'
 
 commands = {
   "Delete Word" => "Control_L+Left Shift+Control_L+Right Delete",
+  "Undo" => "Control_L+z",
+  "Redo" => "Control_L+Shift+z",
+  "Switch windowS" => "Alt_L+Tab",
+  "Switch Workspace" => "Control_L+Alt+Right",
 }
 
 display = ->(line) { STDOUT.write("#{line}\n") }
