4d56803b11287cff8aff1ad7ec266c4663f8d1df
Facundo Javier Gelatti <javiergelatti@gmail.com>
2022-02-26 18:48:30 -0300

    Update hud to work with new keyboard model (without autodetect feature, yet)

--- hud.sh
+++ hud.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 # Setup TTY for Arduino
 # see https://playground.arduino.cc/Interfacing/LinuxTTY/
-stty -F /dev/ttyACM0 cs8 9600 ignbrk \
+stty -F /dev/ttyACM2 cs8 9600 ignbrk \
     -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 
-cat /dev/ttyACM0 \
+tail -f /dev/ttyACM2 \
     | ruby hud.rb \
     | osd_cat --align left --pos bottom --lines 1 --outline 1 --color white --font -bitstream-*-medium-r-*-*-*-120-*-*-m-*-*-0 # see xfontsel
